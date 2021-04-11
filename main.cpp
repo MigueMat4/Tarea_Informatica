cf7e250e83a6db7971b9eed1e65c72744fd2af71
Selvin Pérez <selvinperezxd@gmail.com>
2021-04-10 20:08:16 -0600

    segundo commit del programa
    Ahi esta el programa que se realizo

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
-// <Ingrese su nombre completo y número de carnet>
+// Selvin Jordan P�rez Xicara 1605319
 #include <iostream>
 
 using namespace std;
-
-int main()
-{
-    cout << "¡Hola estudiantes!" << endl;
-    return 0;
+int main(){
+	int numero;
+	int sumatoria;
+	sumatoria = 0;
+	
+	do 
+	{//parte del ciclo 
+		cout << "ingrese un numero : ";
+		cin >> numero;
+		sumatoria = sumatoria + numero;		
+	} while (numero > 0); 
+	sumatoria = sumatoria - numero;
+	
+	
+	cout << " el resutado es: " << sumatoria;
+	
+	return 0;
 }
