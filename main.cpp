120d65a0566b23769cebf94f90d6870bf06ce9d0
Dickson-Cotom <78808712+Dickson-Cotom@users.noreply.github.com>
2021-04-11 22:11:55 -0600

    Programa
    Se agrega programa hecho en la clase de progra

--- main.cpp
+++ main.cpp
@@ -1,10 +1,34 @@
-// <Ingrese su nombre completo y número de carnet>
+// <Dickson Everardo Cotom De León -1669921>
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "¡Hola estudiantes!" << endl;
-    return 0;
+	cout << "¡Hola estudiantes!" << endl;
+	return 0;
 }
+
+int main() {
+	int n;
+	int divisible;
+	int contador;
+	contador = 1;
+
+	//Se pedira al usuario que ingrese un numero
+	cout << "Ingrese el numero: " << endl;
+	cin >> n;
+
+	// se tratara de encotrar por que numeros es divisible
+	do {
+		for (int a = 1; a <= n; a++) {
+			if (n % a == 0) {
+				cout << a << " ";
+				contador == n;
+			}
+		}
+	} while (contador == n);
+
+}
+
+
