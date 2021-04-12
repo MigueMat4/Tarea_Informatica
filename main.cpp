4f9f983f06a8ad8788aa2f8a92d067899908b45a
Leonela Motta <69704985+Leonela11@users.noreply.github.com>
2021-04-11 21:49:46 -0600

    Archvo gitgnore
    Gitignore

--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
-// <Ingrese su nombre completo y número de carnet>
+// Britany Leonela Alvarez Motta 1636921
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "¡Hola estudiantes!" << endl;
-    return 0;
+	int numero;
+	cout << "Ingrese un numero entero: ";
+	cin >> numero;
+	int multiplos;
+	int suma;
+	int res;
+	int cociente;
+	cociente = 0;
+	multiplos = 0;
+	suma = 0;
+	res = 0;
+	//Utilizaremos un ciclo for para poder realizar las operaciones
+	for (int i = 1; i < numero; i++) {
+		res = numero / i;
+		cociente = numero % i;
+		//Un if para hacer la comparacion del resultado del cociente
+		if (cociente == 0) {
+			multiplos++;
+			cout << i << endl;
+			suma = suma + i;
+		}
+	}
+	//Haremos la comparacion para ver si el numero es perfecto, deficiente o abundante.
+	if (suma == numero) {
+		cout << "El numero es perfecto" << endl;
+	}
+	else if (suma > numero) {
+		cout << "El numero es abundante";
+	}
+	else if (suma < numero) {
+		cout << "El numero es deficiente" << endl;
+	}
+	else {
+		cout << "Lo que ingreso no es un numero entero";
+	}
 }
