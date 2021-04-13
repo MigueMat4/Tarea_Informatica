a125d9c65a52d4b5400cd770b9096366d0361a7a
Ruben-online <78996839+Ruben-online@users.noreply.github.com>
2021-04-13 00:52:27 -0600

    commit del programa
    no se quitaron las lineas de codigo que se habian creado anteriormente a la realizacion del programa "factorial"

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
-// <Ingrese su nombre completo y número de carnet>
+// Ruben Fernando Espinoza Fernandez Carne:1525620
 #include <iostream>
 
 using namespace std;
 
+
 int main()
 {
-    cout << "¡Hola estudiantes!" << endl;
-    return 0;
-}
+	int numero, factorial = 1;
+	cout << "Ingrese numero a calcular: ";
+	cin >> numero;
+
+	for (int i = 1; i <= numero; i++)
+	{
+		factorial = factorial * i;
+	}
+
+	cout << "El factorial del numero es " << endl;
+	cout << numero << "! = " << factorial << endl << endl;
+
+	system("pause");
