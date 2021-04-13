f142e1652878b715346c9b7df064cf0be6f04509
Diana Morales <ruanodiana03@gmail.com>
2021-04-12 22:18:49 -0600

    Venta de teclados
    Este programa permite saber cuantos teclados se han vendido y el total.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
-// <Ingrese su nombre completo y número de carnet>
+// Diana Melissa Morales Ruano  
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
-    cout << "¡Hola estudiantes!" << endl;
-    return 0;
+	//venta de teclados
+	int a, p1;
+
+
+
+	cout << "Ingrese la cantidad de teclados que desea comprar";
+	cin >> a;
+
+	if (a>8)
+	{
+		p1 = (100 * a);
+
+		cout << "El total de su compra es:";
+		cout << p1;
+	}
+	else if (a >= 4 && a <= 8)
+	{
+		p1 = (110 * a);
+		cout << "El total de su compra es:";
+		cout << p1;
+	}
+	else
+	{
+		p1 = (150 * a);
+		cout << "El total de su compra es:";
+		cout << p1;
+
+	}
+
+	cout << endl;
+	system("pause");
+
 }
